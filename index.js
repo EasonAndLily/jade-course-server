d7b6487e75162a5e55b2cf0f941ca1a165bd6350
Eason Li <lijinghua@odc.cmbchina.cn>
2020-05-03 23:27:25 +0800

    Use package cors to cross-domain access

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from 'express';
 import Router from './src/router.js';
 import yargs from 'yargs';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 new Router(app);
 
 mongoose.connect('mongodb://localhost:27017/jade', {
